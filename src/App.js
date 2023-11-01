7d9ba2e3c5cca76152f0206103a941efe55260f4
Padmasripriya <padmasripriya17@gmail.com>
2023-11-01 15:14:23 -0500

    Added two column component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import MenuComponent from './modules/MenuComponent';
 import SectionComponent from './modules/SectionComponent';
 import CtaComponent from './modules/CTAComponent';
 import HeaderComponent from './modules/HeaderComponent';
+import TwoColumnComponent from './modules/TwoColumnComponent';
 
 function App() {
   return (
     <div className="App container">
       <MenuComponent />
       <HeaderComponent />
+      <TwoColumnComponent />
       <SectionComponent />
       <CtaComponent />
     </div>
