6fa1f1864cb17e36fb0f3ef21da5b0d58ed39b25
Padmasripriya <padmasripriya17@gmail.com>
2023-11-30 17:20:49 -0600

    added mobile css for header component

--- src/modules/HeaderComponent.js
+++ src/modules/HeaderComponent.js
@@ -3,16 +3,28 @@ import headerImage from '../images/illustration-intro.svg';
 
 function HeaderComponent() {
     return(
-        <div className="d-md-inline-flex">
-            <div className="left">
-                <h1>Bring everyone together to build better products.</h1>
-                <p>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
-                <a href="#" className="button">Get Started</a>
+        <>
+            <div className="d-md-inline-flex d-none">
+                <div className="left">
+                    <h1 className="fw-bold">Bring everyone together to build better products.</h1>
+                    <p>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
+                    <a href="#" className="button">Get Started</a>
+                </div>
+                <div className="right">
+                    <img src={headerImage} alt="header image" />
+                </div>
             </div>
-            <div className="right">
-                <img src={headerImage} alt="header image" />
+            <div class="d-md-none d-block mb-5 mt-5">
+                <div>
+                    <img src={headerImage} alt="header image" />
+                </div>
+                <div className="text-center">
+                        <h1 className="fw-bold">Bring everyone together to build better products.</h1>
+                        <p>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
+                        <a href="#" className="button">Get Started</a>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
